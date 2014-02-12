753dc51c102b857f63bd5b58415a6dee35a0fd3e
craigscode24 <craigscode24@users.noreply.github.com>
2014-02-11 18:18:28 -0600

    Added fib sequence file

---
+++ FibTest/FibTest.java
@@ -0,0 +1,21 @@
+
+
+public class FibTest {
+
+   public static void main(String[] args) {
+      // 1, 1, 2, 3, 5, 8
+      for (int i = 0; i <= 50; i++)
+         System.out.println("Fib value for elem " + i + " = " + fib(i));
+   }
+
+   private static long fib(int elem) {
+      if (elem == 0)
+         return 0;
+
+      if (elem == 1)
+         return 1;
+
+      return fib(elem - 1) + fib(elem - 2);
+   }
+
+}
